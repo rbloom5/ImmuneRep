c45312c673dac7a2168a275d6e7a0e35b8f64276
rbloom5 <rbloom5@gmail.com>
2015-04-30 13:12:37 -0700

    wrote out pseudo code for updating rep stats

--- rep_features/Rep_feature_analysis.py
+++ rep_features/Rep_feature_analysis.py
@@ -1,18 +1,47 @@
 #!/usr/bin/python
 
 
+from rep_stat_functions import *
+
+def update_rep_stats(reps = default):
+	if reps == default:
+		reps = [
+
+		###  I'm thinking this is a list of all the id's of all of our reps
+
+		]
 
 
-class Rep_stats:
+	#run through each rep, load it in, update features dict with any new features, and dump it into a new json
 
-	def __init__(self, file_locs):
-		self.features={}
+	for rep in reps:
+		features_dict = load rep json
+		Rep = load rep Rep_seq_object
 
-		for f in file_locs:
-			f_id = f.split('/')[-1][:-4]
-			with open(f) as rep:
-				cur_rep = pickle.load(rep)
-				self.features[f_id] = cur_rep.features
 
-	
+		## now loop through all of our functions and see if it is in the json
+		## if not, then calculate it
+
+		if feature_name not in features_dict:
+			features_dict[feature_name] = function_that_calculates_feature(Rep) #function stored in rep_stat_functions
+		
+
+		if other_feature_name not in features_dict:
+			features_dict[other_feature_name] = function_that_calculates_other_feature(Rep)
+
+
+			#  etc. etc. for all interesting funcitons
+
+
+		# once we run through everything, dump the updated features dict into a new json
+		import json
+		with open(outpath,'wb') as outfile:
+			json.dump(self.features_dict,outfile,indent=1,sort_keys=True)
+
+
+
+
+
+
+
 

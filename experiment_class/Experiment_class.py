bfeb527b74c4963bac691ba73f1f9a813dec5e77
ramanktalwar <ramancoke.3@gmail.com>
2015-05-18 16:50:41 -0700

    Updated Tree plots

--- experiment_class/Experiment_class.py
+++ experiment_class/Experiment_class.py
@@ -1,26 +1,31 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import seaborn as sns
 import pandas as pd 
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
 class Experiment:
-	def __init__(self,groups,defaultgroupspath='defaultgroups.txt'):
-		defaultgroups=json.load(open(defaultgroupspath)) 
+	def __init__(self,groups):
+
+		#Opens up an S3 connection and sets buckets
+		#dont share the strings below with anyone!
+		conn = S3Connection('AKIAJ2TEUHQV2LHU7XQQ','VKzoYINBlvZi5uiAIeAnJG5fgLedQPFrmMCpSfBp')
+		json_buck = conn.get_bucket('rep-seq-jsons')
+		info_buck = conn.get_bucket('info-docs')
 
 		#Checks for groups with no input samples and looks for a default
+		info_buck.get_key("ExperimentClassDefaultGroups.txt").get_contents_to_filename("ExperimentClassDefaultGroups.txt")
+		with open("ExperimentClassDefaultGroups.txt") as default_groups_location:
+			defaultgroups=json.load(default_groups_location) 
+
 		for igroup, group in enumerate(groups):
 			if 'samples' not in group:
 				groups[igroup]['samples']=defaultgroups[group['name']]
 
 		#Downloads all the JSONs from AWS to the current directory
-		#dont share the strings below with anyone!
-		conn = S3Connection('AKIAJ2TEUHQV2LHU7XQQ','VKzoYINBlvZi5uiAIeAnJG5fgLedQPFrmMCpSfBp')
-		json_buck = conn.get_bucket('rep-seq-jsons')
-		json_names = [str(key.name) for key in json_buck.list()]
-
 		print "downloading jsons"
 		for key in json_buck.list():
 			key.get_contents_to_filename('tempJSONs/'+key.name)
@@ -119,7 +124,19 @@ class Experiment:
 
 	def clone_bars(self):
 
-		self.clone_DF.plot(kind='bar', stacked='True');
+		self.clone_DF.plot(x='Sample', kind='bar', figsize=(20,6));
+
+	def clone_cum_dist(self):
+
+		colors = sns.color_palette("hls", len(self.groups))
+		patches = []
+		for igroup, group in enumerate(self.groups):
+		    patches.append(mpatches.Patch(color=colors[igroup], label=group['name']))
+		    for sample in group['samples']:
+		        data = self.groups[igroup]['sample data'][sample]['clone_distribution']
+		        plt.plot(np.array(data).cumsum(), color=colors[igroup])
+		plt.legend(handles=patches, loc=4);
+
 
 	def split_SHM_DF(self):
 		SHM_columns = ['Antibody', 'Group', 'Sample', 'Data']
@@ -139,3 +156,45 @@ class Experiment:
 
 		try: sns.factorplot('Sample', y='Data', hue='Group', data=self.SHM_DF_split, col='Antibody'); 	
 		except: print "run self.split_SHM_DF"
+
+	def pruned_vs_full(self):
+
+		colors = sns.color_palette("hls", len(self.groups))
+		plt.figure(figsize=(20,6))
+
+		for igroup, group in enumerate(self.groups):
+		    for sample in group['samples']:
+		        fullDF = pd.DataFrame.from_dict(self.groups[igroup]['sample data'][sample]['Full_Tree_Size'])
+		        prunedDF = pd.DataFrame.from_dict(self.groups[igroup]['sample data'][sample]['Pruned_Tree_Size'])
+		        
+		        prune = prunedDF.values.reshape(702)
+		        full = fullDF.values.reshape(702)
+		        
+		        plt.scatter(prune, full, s=10, c=colors[igroup]);
+		        
+		plt.show()
+
+	def pruned_vs_total_reads(self):
+
+		colors = sns.color_palette("hls", len(self.groups))
+		plt.figure(figsize=(20,6))
+
+		for igroup, group in enumerate(self.groups):
+		    for sample in group['samples']:
+		        fullDF = pd.DataFrame.from_dict(self.groups[igroup]['sample data'][sample]['Full_Tree_Size'])
+		        #prunedDF = pd.DataFrame.from_dict(test.groups[igroup]['sample data'][sample]['Pruned_Tree_Size'])
+		        
+		        tree_size_list = []
+		        total_reads = []
+		        for key in self.groups[igroup]['sample data'][sample]['VJ_freqs']:
+		            V = key.split('_')[0]
+		            J = key.split('_')[1]
+
+		            tree_size_list.append(fullDF[J].loc[V])
+		            total_reads.append(test.groups[igroup]['sample data'][sample]['VJ_freqs'][key])
+		            
+		        plt.scatter(tree_size_list, total_reads, s=10, c=colors[igroup]);
+		        
+		plt.show()
+		        
+        
